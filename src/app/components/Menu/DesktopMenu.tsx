9c42beb2b7e38a3fae2afee11f924851190a7da4
Jerome Trottier <jerome.trottier@hotmail.ca>
2025-02-12 21:47:42 -0500

    feat: added donate button to navigation

--- src/app/components/Menu/DesktopMenu.tsx
+++ src/app/components/Menu/DesktopMenu.tsx
@@ -1,15 +1,31 @@
+import { Link } from "@/i18n/routing"
 import { TMenu } from "./interface"
 import MenuItem from "./MenuItem"
+import { useTranslations } from "next-intl"
 
 const DesktopMenu = (props: TMenu) => {
 
   const { menuItems } = props
 
+  const t = useTranslations('nav')
+
+  const donateLabel = t('donate')
+
   return (
     <nav className='hidden md:block text-white fixed top-5 right-5'>
       <ul className='flex gap-4 text-xl'>
         {menuItems.map((item) => <MenuItem key={item.href} {...item} />)}
+        <li>
+          <Link
+            target="_blank"
+            className="bg-primary px-4 py-2 rounded-full font-bebas hover:bg-purple-950 transition-all" 
+            href="https://www.jedonneenligne.org/fdets/campagne/ets/challenges/view/832ba095-53f5-11ee-ac26-001dd8b75df7"
+          >
+              {donateLabel}
+          </Link>
+        </li>
       </ul>
+      
     </nav>
   )
 }
