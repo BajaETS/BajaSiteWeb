6240925f40c80cd44ae8d977dffbdea736961f98
oskar <xavier.orsat-parker.1@ens.etsmtl.ca>
2025-10-22 13:18:48 -0400

    Change all occurrences of img to Image, auto optimizes

--- src/app/components/MiniLogo/index.tsx
+++ src/app/components/MiniLogo/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "@/i18n/routing";
+import Image from "next/image";
 
 export function MiniLogo() {
   return (
     <div className='fixed top-0 left-0 p-4 z-20 w-auto'>
       <button>
         <Link href="/">
-          <img src='/logo2025.png' alt='LogoBaja' height="78" width="174" />
+          <Image src='/logo2025.png' alt='LogoBaja' height="174" width="174" />
         </Link>
       </button>
     </div>
