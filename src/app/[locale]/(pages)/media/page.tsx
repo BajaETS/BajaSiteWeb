21db6234aee2078e6e0bad30d0d1265cca23dcda
vawerekax <123829325+vawerekax@users.noreply.github.com>
2025-11-14 15:11:29 -0500

    carousel working, need transition and previews

--- src/app/[locale]/(pages)/media/page.tsx
+++ src/app/[locale]/(pages)/media/page.tsx
@@ -8,12 +8,13 @@ export default function Media() {
   const t = useTranslations('pages.media');
 
   const arizona2025 = [  "arizona2025/IMG_6342.JPG","arizona2025/IMG_6366.JPG",  "arizona2025/IMG_6373.JPG",  "arizona2025/IMG_6377.JPG",  "arizona2025/IMG_6390.JPG",  "arizona2025/IMG_6392.JPG",  "arizona2025/IMG_6407.JPG",  "arizona2025/IMG_6420.JPG",  "arizona2025/IMG_6426.JPG",  "arizona2025/IMG_6455.JPG",  "arizona2025/IMG_6463.JPG",  "arizona2025/IMG_6486.JPG",  "arizona2025/IMG_6491.JPG",  "arizona2025/IMG_6496.JPG",  "arizona2025/IMG_6551.JPG",  "arizona2025/IMG_6556.JPG",  "arizona2025/IMG_6564.JPG",  "arizona2025/IMG_6565.JPG",  "arizona2025/IMG_6576.JPG",  "arizona2025/IMG_6603.JPG",  "arizona2025/IMG_6604.JPG",  "arizona2025/IMG_6613.JPG",  "arizona2025/IMG_6622.JPG",  "arizona2025/IMG_6625.JPG",  "arizona2025/IMG_6629.JPG",  "arizona2025/IMG_6645.JPG",  "arizona2025/IMG_6649.JPG",  "arizona2025/IMG_6657.JPG",  "arizona2025/IMG_6665.JPG",  "arizona2025/IMG_6672.JPG",  "arizona2025/IMG_6675.JPG",  "arizona2025/IMG_6694.JPG",  "arizona2025/IMG_6969.JPG",  "arizona2025/IMG_6987.JPG",  "arizona2025/IMG_7030.JPG",  "arizona2025/IMG_7031.JPG",  "arizona2025/IMG_7116.JPG",  "arizona2025/IMG_7245.JPG"];
-  const maryland2025 = ["IMG_7753.JPG","IMG_7782.JPG","IMG_7793.JPG","IMG_7835.JPG","IMG_7841.JPG","IMG_7851.JPG","IMG_7880.JPG","IMG_7908.JPG","IMG_7933.JPG","IMG_7936.JPG","IMG_8088.JPG","IMG_8137.JPG","IMG_8161.JPG","IMG_8177.JPG","IMG_8181.JPG","IMG_8209.JPG","IMG_8224.JPG","IMG_8284.JPG","IMG_8347.JPG","IMG_8371.JPG","IMG_8381.JPG","IMG_8446.JPG","IMG_8492.JPG","IMG_8579.JPG","IMG_8602.JPG","IMG_8627.JPG","IMG_8647.JPG","IMG_8768.JPG","IMG_8777.JPG"];
+  const maryland2025 = ["maryland2025/IMG_7753.JPG","maryland2025/IMG_7782.JPG","maryland2025/IMG_7793.JPG","maryland2025/IMG_7835.JPG","maryland2025/IMG_7841.JPG","maryland2025/IMG_7851.JPG","maryland2025/IMG_7880.JPG","maryland2025/IMG_7908.JPG","maryland2025/IMG_7933.JPG","maryland2025/IMG_7936.JPG","maryland2025/IMG_8088.JPG","maryland2025/IMG_8137.JPG","maryland2025/IMG_8161.JPG","maryland2025/IMG_8177.JPG","maryland2025/IMG_8181.JPG","maryland2025/IMG_8209.JPG","maryland2025/IMG_8224.JPG","maryland2025/IMG_8284.JPG","maryland2025/IMG_8347.JPG","maryland2025/IMG_8371.JPG","maryland2025/IMG_8381.JPG","maryland2025/IMG_8446.JPG","maryland2025/IMG_8492.JPG","maryland2025/IMG_8579.JPG","maryland2025/IMG_8602.JPG","maryland2025/IMG_8627.JPG","maryland2025/IMG_8647.JPG","maryland2025/IMG_8768.JPG","maryland2025/IMG_8777.JPG"];
   const carolina2025 = [];
 
   return (
     <Page>
       <Carousel slides={arizona2025}/>
+      <Carousel slides={maryland2025}/>
     </Page>
   );
 }
--- src/app/components/Carousel/index.tsx
+++ src/app/components/Carousel/index.tsx
@@ -9,18 +9,39 @@ export default function Carousel({ slides }: { slides: string[] }) {
     const [currentIndex, setCurrentIndex] = useState(0);
     
     return (
-      <>
-        <button onClick={() => setCurrentIndex((currentIndex - 1 + images.length) % images.length)}>
-          previous
+      <div className="w-full h-[50vh] flex items-center p-4">
+        <button 
+          className="hover:scale-125 transition-transform duration-200" 
+          onClick={() => setCurrentIndex((currentIndex - 1 + images.length) % images.length)}
+        >
+          <Image
+            src="/icons/arrow_left.png"
+            alt={t('previous')}
+            width={48}
+            height={48}
+          />
         </button>
-        <div className="relative w-full overflow-hidden">
-          <div className="flex transition-transform duration-500" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
-            <Image key={currentIndex} src={images[currentIndex]} alt={t('carousel image', { index: currentIndex })} width={500} height={300} />
-          </div>
+        <div className="relative w-full h-full overflow-hidden flex justify-center items-center">
+          <Image
+            key={currentIndex}
+            quality={40}
+            src={images[currentIndex]}
+            alt="image"
+            fill
+            style={{ objectFit: 'contain' }}
+          />
         </div>
-        <button onClick={() => setCurrentIndex((currentIndex + 1) % images.length)}>
-          next
+        <button 
+          className="hover:scale-125 transition-transform duration-200" 
+          onClick={() => setCurrentIndex((currentIndex + 1) % images.length)}
+        >
+          <Image
+            src="/icons/arrow_right.png"
+            alt={t('next')}
+            width={48}
+            height={48}
+          />
         </button>
-      </>
+      </div>
     );
 }
